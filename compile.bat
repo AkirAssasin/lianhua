1f2393923d412d50f507c889de66dfdd8c60ecc0
akirassasin <AkirAssasin@users.noreply.github.com>
2021-11-22 23:54:33 +0800

    Minor corrections, fixed act 3 diety dialogue, added proofread edition

--- compile.bat
+++ compile.bat
@@ -1,3 +1,14 @@
+:: hide input commands
 @echo off
-tweego -o output.html src
+
+:: compile actual output
+tweego -o index.html src
+
+:: compile proofread edition
+tweego -o proofread.html -f paperthin-1 src
+
+:: zip up output with asset and proofread edition
+tar -a -c -v -f lianhua.zip index.html assets proofread.html
+
+:: wait for user
 pause
